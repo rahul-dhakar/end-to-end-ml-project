55c94903425ea970c07e8a0a23ac26bac6f1679e
Rahul Dhakar <rahulcs849@gmail.com>
2026-07-11 16:11:36 +0530

    Model Trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,27 +1,30 @@
 import sys
 from dataclasses import dataclass
-import numpy as np
+
+import numpy as np 
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
 from src.exception import CustomException
 from src.logger import logging
 import os
+
 from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path = os.path.join('artifacts','proprocessor.pkl')
+    preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
 
 class DataTransformation:
     def __init__(self):
-        self.data_transformation_config = DataTransformationConfig()
-    
+        self.data_transformation_config=DataTransformationConfig()
+
     def get_data_transformer_object(self):
         '''
-        This function is responsible for data transformation
+        This function si responsible for data trnasformation
         
         '''
         try:
@@ -69,7 +72,7 @@ class DataTransformation:
         
         except Exception as e:
             raise CustomException(e,sys)
-    
+        
     def initiate_data_transformation(self,train_path,test_path):
 
         try:
